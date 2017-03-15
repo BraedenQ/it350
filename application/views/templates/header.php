4693578d632304985d1cbe485d589463f9f5f0e8
Seth <seth.pacheco@gmail.com>
2017-03-15 12:32:26 -0700

    Made navbar responsive

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -9,32 +9,41 @@
 				<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
         </head>
         <body>
-	        <style type="text/css">
-	        	.navbar .navbar-nav {
-				  display: inline-block;
-				  float: none;
-				  vertical-align: top;
-				}
+        <style type="text/css">
+        	.navbar .navbar-nav {
+			  display: inline-block;
+			  float: none;
+			  vertical-align: top;
+			}
 
-				.navbar .navbar-collapse {
-				  text-align: center;
-				}
-	        </style>
+			.navbar .navbar-collapse {
+			  text-align: center;
+			}
+        </style>
 
         	<nav class="navbar navbar-inverse">
 			  	<div class="container-fluid">
-				    <!-- Collect the nav links, forms, and other content for toggling -->
-				    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-				      	<ul class="nav navbar-nav">
-					      	<!-- Brand and toggle get grouped for better mobile display -->
+			    
+				    <div class="navbar-header">
+				      	<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+					        <span class="sr-only">Toggle navigation</span>
+					        <span class="icon-bar"></span>
+					        <span class="icon-bar"></span>
+					        <span class="icon-bar"></span>
+				      	</button>
+				    </div>
+
+			    <!-- Collect the nav links, forms, and other content for toggling -->
+			    	<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			      		<ul class="nav navbar-nav">
 					      	<li><a class="navbar-brand" href="#"><?php echo $title; ?></a></li>
 					        <li class=""><a href="#">Link <span class="sr-only">(current)</span></a></li>
 					        <li><a href="#">Link</a></li>
 					        <li><a href="#">Link</a></li>
-					      </ul>
-				      	<ul class="nav navbar-nav navbar-right">
-				        	<li><a href="#">Link</a></li>
-				      	</ul>
-				    </div><!-- /.navbar-collapse -->
+			      		</ul>
+			      		<ul class="nav navbar-nav navbar-right">
+			        		<li><a href="#">Link</a></li>
+			      		</ul>
+			    	</div><!-- /.navbar-collapse -->
 			  	</div><!-- /.container-fluid -->
 			</nav>
