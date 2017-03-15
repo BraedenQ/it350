9e70f5caeb1463b4a217ef8af596f7c0a4e2895f
Seth <seth.pacheco@gmail.com>
2017-03-15 12:40:18 -0700

    better footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,6 +1,39 @@
-                <footer style="background-color: grey; position: absolute; left: 0; bottom: 0; height: 100px; width: 100%; overflow:hidden;">
-                	<em>&copy; 2015</em>
-                </footer>
+                <style type="text/css">
+                	html {
+					  position: relative;
+					  min-height: 100%;
+					}
+					body {
+					  /* Margin bottom by footer height */
+					  margin-bottom: 60px;
+					}
+					.footer {
+					  position: absolute;
+					  bottom: 0;
+					  width: 100%;
+					  /* Set the fixed height of the footer here */
+					  height: 60px;
+					  background-color: #f5f5f5;
+					}
+
+					/* This is not required for sticky footer Just styling for footer */
+					body > .container {
+					  padding: 60px 15px 0;
+					}
+					.container .text-muted {
+					  margin: 20px 0;
+					}
+					.footer > .container {
+					  padding-right: 15px;
+					  padding-left: 15px;
+					}
+                </style>
+
+                <footer class="footer">
+			      	<div class="container">
+			        	<p class="text-muted">Place sticky footer content here.</p>
+			      	</div>
+			    </footer>
         </body>
 </html>
 
