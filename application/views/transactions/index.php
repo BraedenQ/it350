2c015e38d826e56be0dfde58e545a311b1cc330a
Seth <seth.pacheco@gmail.com>
2017-03-28 23:38:09 -0600

    Making the transactions page look universal...

--- application/views/transactions/index.php
+++ application/views/transactions/index.php
@@ -1,17 +1,31 @@
-<h2><?php echo $title; ?></h2>
-<table>
-	<tr>
-		<th>Transaction ID</th>
-		<th>Type</th>
-		<th>Amount</th>
-	</tr>
-	<?php foreach ($transactions as $transaction): ?>
-		<tr>
-	       <td><?php echo "{$transaction['transID']}"; ?></td>
-	       <td><?php echo "{$transaction['type']}"; ?></td>
-	       <td><?php echo "{$transaction['amount']}"; ?></td> 
-       </tr>   
-	<?php endforeach; ?>
-</table>
-<br/>
-<p><a href="<?php echo site_url('editTransactions'); ?>">Edit Transactions</a></p> 
+<div class="row">
+	<div class="col-md-3"></div>
+	<div class="col-md-6">
+		<h2 style="text-align: center;"><?php echo $title; ?></h2>
+	</div>
+	<div class="col-md-3"></div>
+</div>
+<div class="row">
+	<div class="col-md-3"></div>
+	<div class="col-md-6">
+		<table class="table table-bordered table-hover">
+			<tr>
+				<th>Transaction ID</th>
+				<th>Type</th>
+				<th>Amount</th>
+			</tr>
+			<?php foreach ($transactions as $transaction): ?>
+				<tr>
+			       <td><?php echo "{$transaction['transID']}"; ?></td>
+			       <td><?php echo "{$transaction['type']}"; ?></td>
+			       <td><?php echo "{$transaction['amount']}"; ?></td> 
+		       </tr>   
+			<?php endforeach; ?>
+		</table>
+	</div>
+	<div class="col-md-3"></div>
+</div>
+
+<div class="col-md-9">
+	<a class="btn btn-primary pull-right" href="<?php echo site_url('editTransactions'); ?>">Edit Transactions</a>
+</div>
