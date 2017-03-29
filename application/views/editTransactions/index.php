8d23f9802aefba8d2c3d97763215ddeab3ba5e97
user_name <email_id>
2017-03-28 17:28:06 -0700

    minor changes

--- application/views/editTransactions/index.php
+++ application/views/editTransactions/index.php
@@ -1,11 +1,14 @@
 Add a Transaction: <br/>
 
 <form name ="userinput" action="editTransactions/add" method="post">
+	Type: <input type="text" name="type"> <br/>
+	Amount: <input type="number" step="0.01" name="amount"> <br/>
+	<input type="submit" value="Submit">
+</form>
 
-
-      Type: <input type="text" name="type"> <br/>
-
-      Amount: <input type="number" step="0.01" name="amount"> <br/>
-
-      <input type="submit" value="Submit">
-  </form>
+Delete a Transaction: <br/>
+<form name ="userinput" action="editTransactions/add" method="post">
+	Type: <input type="text" name="type"> <br/>
+	Amount: <input type="number" step="0.01" name="amount"> <br/>
+	<input type="submit" value="Submit">
+</form>
