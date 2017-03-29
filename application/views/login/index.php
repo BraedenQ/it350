22282940f49bd77510befac8a86e1f67537eff6e
Seth <seth.pacheco@gmail.com>
2017-03-28 23:28:09 -0600

    Making the login page better for the eyes...

--- application/views/login/index.php
+++ application/views/login/index.php
@@ -1,15 +1,75 @@
- <body>
-   <h1>Simple Login with CodeIgniter</h1>
-   <p>The creds are admin:admin. Also make sure you go to clinic.com/index.php to make the application work.</p>
-   <?php echo validation_errors(); ?>
-   <?php echo form_open('verifylogin'); ?>
-     <label for="username">Username:</label>
-     <input type="text" size="20" id="username" name="username"/>
-     <br/>
-     <label for="password">Password:</label>
-     <input type="password" size="20" id="passowrd" name="password"/>
-     <br/>
-     <input type="submit" value="Login"/>
-   </form>
- </body>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<style type="text/css">
+  .vcenter {
+    display: inline-block;
+    vertical-align: middle;
+    float: none;
+  }
+
+  .topSpace {
+    margin-top: 10px !important;
+  }
+
+  .btn-outline-primary {
+    color: #0275d8;
+    background-image: none;
+    background-color: transparent;
+    border-color: #0275d8;
+    transition: all 2s;
+  }
+    .btn-outline-primary:hover {
+      color: white;
+      background-color: #0275d8;
+      width: 25%;
+    }
+
+  .center {
+    text-align: center;
+  }
+
+  .form-actions {
+    margin: 0;
+    background-color: transparent;
+    text-align: center;
+  }
+
+  .customFormLocation {
+    position: relative;
+    top: 20%;
+  }
+ </style>
+
+<body>
+  <div class="customFormLocation">
+    <div class="row">
+      <div class="col-md-4"></div>
+      <div class="col-md-4">
+        <h2>Simple Login with CodeIgniter</h2>
+        <p>The creds are admin:admin. Also make sure you go to clinic.com/index.php/login to make the application work.</p>
+        <?php echo validation_errors(); ?>
+      </div>
+      <div class="col-md-4"></div>
+    </div>
+    <?php echo form_open('verifylogin'); ?>
+    <div class="row">
+      <div class="col-md-4"></div>
+      <div class="col-md-4">
+        <!-- <label for="username">Username:</label> -->
+        <input class="form-control" type="text" size="20" id="username" name="username" placeholder="Username" />
+      
+        <!-- <label for="password">Password:</label> -->
+        <input class="form-control topSpace" type="password" size="20" id="passowrd" name="password" placeholder="Password" />
+        <div class="form-actions topSpace">
+        <button type="submit" class="btn btn-outline-primary" style="text-align: center;">Login</button>
+        </div>
+      </div>
+      <div class="col-md-4"></div>
+    </div>
+    </form>
+  </div>
+
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+  <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+</body>
 </html>
